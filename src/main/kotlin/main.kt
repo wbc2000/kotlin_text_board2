489577764056f002cea1d73fd7ceb17b319a119d
wbc2000 <woobaekc@gmail.com>
2021-04-22 00:32:21 +0900

    시스템 종료기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("안녕하세요.")
+    println("게시판 관리 프로그램 시작")
+    while ( true ) {
+        print("명령어) ")
+        val command = readLine()
+        println("입력한 명령어 : $command")
+
+        if ( command == "system exit" ) {
+            println("프로그램을 종료합니다.")
+            break
+        }
+    }
+
+    println("== 게시판 관리 프로그램 끝 ==")
 }
